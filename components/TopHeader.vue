d607c5b5f9afbb159d57fbde54473d0ad0b629c4
NiheiKeita <86122925+NiheiKeita@users.noreply.github.com>
2023-10-30 19:29:43 +0900

    マイページ作成途中

--- components/TopHeader.vue
+++ components/TopHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="sticky top-0 flex w-full justify-between bg-ThemaColor">
+  <div
+    class="sticky top-0 flex w-full justify-between border-b-[1px] border-white bg-ThemaColor"
+  >
     <div class="flex justify-center">
       <img class="m-2 h-14" src="/images/icon_web.png" />
       <p class="flex items-center justify-center text-2xl font-bold text-black">
--- pages/my_page/my_page.vue
+++ pages/my_page/my_page.vue
@@ -1,5 +1,79 @@
-<script setup language="ts"></script>
+<script setup language="ts">
+  // TODO(問題表示)
+
+  const questions = [
+    {
+      id: 1,
+      name: '解答１',
+      question_title: 'question_title',
+      question_body: 'question_body',
+    },
+    {
+      id: 1,
+      name: '解答１',
+      question_title: 'question_title',
+      question_body: 'question_body',
+    },
+    {
+      id: 1,
+      name: '解答１',
+      question_title: 'question_title',
+      question_body: 'question_body',
+    },
+  ];
+  const select = ref('post');
+  const profileEditClick = () => {};
+  const createQuestionClick = () => {};
+  const tabClick = (tabName) => {
+    console.log(tabName);
+    select.value = tabName;
+  };
+</script>
 
 <template>
-  <div>マイページ</div>
+  <div class="md:mx-[20%]">
+    <div class="my-4 flex items-center justify-center">マイページ</div>
+    <div class="my-4 flex items-center justify-center">フォロワーとか</div>
+    <div class="my-4 flex items-center justify-center">ID</div>
+    <div class="my-4 flex items-center justify-evenly">
+      <ButtonDefaultThema
+        msg="プロフィールを編集する"
+        @click="profileEditClick"
+      />
+      <ButtonDefaultThema msg="投稿する" @click="createQuestionClick" />
+    </div>
+    <div class="my-4 flex items-center justify-center">
+      <div class="flex h-10 w-full">
+        <ButtonTabDefault
+          msg="投稿"
+          :select="select == 'post'"
+          @click="tabClick('post')"
+        />
+        <ButtonTabDefault
+          msg="履歴"
+          :select="select == 'history'"
+          @click="tabClick('history')"
+        />
+        <ButtonTabDefault
+          msg="マイリスト"
+          :select="select == 'mylist'"
+          @click="tabClick('mylist')"
+        />
+        <ButtonTabDefault
+          msg="保存"
+          :select="select == 'save'"
+          @click="tabClick('save')"
+        />
+      </div>
+    </div>
+    <div>
+      <div
+        v-for="(question, i) in questions"
+        :key="i"
+        class="my-4 flex items-center justify-center"
+      >
+        <QuestionBoardDefault class="w-4/5 md:w-1/2" :question="question" />
+      </div>
+    </div>
+  </div>
 </template>
